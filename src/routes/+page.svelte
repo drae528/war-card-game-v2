936f9c59410398666e24fa93be5c0cae2d47105b
drae <169489544+drae528@users.noreply.github.com>
2025-05-07 12:35:09 -0500

    finished logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,27 +4,43 @@
 
 	let { data } = $props();
 
-	let playerDeck = $state(data.playerDeck);
 	let enemyDeck = $state(data.enemyDeck);
+	let playerDeck = $state(data.playerDeck);
 	$effect(() => {
-		playerDeck = data.playerDeck || [];
 		enemyDeck = data.enemyDeck || [];
+		playerDeck = data.playerDeck || [];
 	});
 
-	let playerPile = $state([]);
 	let enemyPile = $state([]);
+	let playerPile = $state([]);
 
-	let pCard = $derived(playerDeck[0] || {});
 	let eCard = $derived(enemyDeck[0] || {});
+	let pCard = $derived(playerDeck[0] || {});
 </script>
 
 <svelte:head><title>War | Card Game</title></svelte:head>
 
 <div class="[ relative ]">
-	<CardPile rank={eCard.rank} value={eCard.value} suit={eCard.suit} />
+	{#each [...enemyDeck].slice(1).reverse() as card, i}
+		<div
+			class="[ absolute -translate-y-1/2 -translate-x-1/2 ] [ drop-shadow-sm ]"
+			style="top: {-1 * i}px; left: {-1 * i}px; z-index: {i};"
+		>
+			<CardPile rank={card.rank} value={card.value} suit={card.suit} />
+		</div>
+	{/each}
 </div>
 <CardElement rank={eCard.rank} value={eCard.value} suit={eCard.suit} />
 <p></p>
 
-<CardPile rank={pCard.rank} value={pCard.value} suit={pCard.suit} />
+<div class="[ relative ]">
+	{#each [...playerDeck].slice(1).reverse() as card, i}
+		<div
+			class="[ absolute -translate-y-1/2 -translate-x-1/2 ] [ drop-shadow-sm ]"
+			style="top: {-1 * i}px; left: {-1 * i}px; z-index: {i};"
+		>
+			<CardPile rank={card.rank} value={card.value} suit={card.suit} />
+		</div>
+	{/each}
+</div>
 <CardElement rank={pCard.rank} value={pCard.value} suit={pCard.suit} />
